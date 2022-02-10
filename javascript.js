c1cf0c9ae31df863961a74e480bc584e59de9f21
yummy314159265 <97650924+yummy314159265@users.noreply.github.com>
2022-02-10 16:46:53 -0600

    readable

--- javascript.js
+++ javascript.js
@@ -1,47 +1,56 @@
-const box = document.getElementById("box");
+//make style declarations more readable
+const box = document.getElementById("box").style;
+let [sizeText, colorText, opacityText, rotationText] = [document.getElementById("size"), document.getElementById("color"), document.getElementById("opacity"), document.getElementById("rotation")];
+
+//initial values for reset
+const [initHeight, initWidth, initColor, initOpaciSty] = [box.height, box.width, box.backgroundColor, box.opacity];
+const [initsizeText, initColorText, initOpacityText, initRotationText] = [sizeText.innerHTML, colorText.innerHTML, opacityText.innerHTML, rotationText.innerHTML];
+
+//variable and function needed for rotation
 let randomDegree = 0;
+const resetDegree = () => randomDegree -= randomDegree%360;
+
+//color array
 const colors = ["Orange", "Red", "Blue", "Green", "Yellow", "Purple", "Magenta", "Brown", "Violet"];
-const randomizer = (range=1) => Math.floor(range * Math.random());
 
-//Randomly grows/shrinks box or resets box size if too big/small
+//function to make random integers 
+const randomizer = (range=1, initial=0) => Math.floor(range * Math.random()) + initial;
+
+//Randomly grows/shrinks within a range
 document.getElementById("button1").onclick = () =>  {
-    //multiplies current size of the box by some random number between 0 and 2
-    let randomSize = randomizer(parseInt(box.style.height)*2); 
-    //if new size is greater than 500px or less than 25px, sets size to 150px x 150px
-    if (randomSize > 500 || randomSize < 25) {
-        randomSize = 150;
-    }
-
-    box.style.height = box.style.width = randomSize + "px";
-    document.getElementById("size").innerHTML = `Size: ${randomSize}px x ${randomSize}px`;
+    let randomSize = randomizer(475, 25);
+    box.height = box.width = randomSize + "px";
+    sizeText.innerHTML = `Size: ${randomSize}px x ${randomSize}px`;
 }
 
 //Randomly changes color of box
 document.getElementById("button2").onclick = () => {    
+    
     //chooses a color from array
     let randomColor = colors[randomizer(colors.length)];    
+    
     //prevents same color from being chosen
-    while (randomColor.toLowerCase() == box.style.backgroundColor.toLowerCase()) {
+    while (randomColor.toLowerCase() == box.backgroundColor.toLowerCase()) {
         randomColor = colors[randomizer(colors.length)];
     }
 
-    box.style.backgroundColor = randomColor;
-    document.getElementById("color").innerHTML = `Color: ${randomColor}`;
+    box.backgroundColor = randomColor;
+    colorText.innerHTML = `Color: ${randomColor}`;
 }
 
 //Randomly changes opacity of box
 document.getElementById("button3").onclick = () => {
     const randomOpacity = randomizer(100);
-    box.style.opacity = randomOpacity/100;
-    document.getElementById("opacity").innerHTML = `Opacity: ${randomOpacity}%`;
+    box.opacity = randomOpacity/100;
+    opacityText.innerHTML = `Opacity: ${randomOpacity}%`;
 }
 
 //Randomly rotates box
 document.getElementById("button4").onclick = () => {
     //set random change in degrees and adds it to global variable, needed to keep box rotating clockwise
     randomDegree += randomizer(180);
-    box.style.transform = `rotate(${randomDegree}deg)`;
-    document.getElementById("rotation").innerHTML = `Rotation: ${randomDegree%360}°`;
+    box.transform = `rotate(${randomDegree}deg)`;
+    rotationText.innerHTML = `Rotation: ${randomDegree%360}°`;
 }
 
 //Randomizes every element
@@ -51,17 +60,9 @@ document.getElementById("button5").onclick = () => {
 
 //Resets box to initial values
 document.getElementById("button6").onclick = () => {
-    const resetDegree = () => randomDegree -= randomDegree%360;
-    box.style = `
-        height:150px; 
-        width:150px; 
-        background-color:Orange; 
-        margin:100px 25px 110px 100px; 
-        transition:all 1.25s; 
-        transform:rotate(${resetDegree()}deg)
-    `;
-    document.getElementById("size").innerHTML = "Size: 150px x 150px";
-    document.getElementById("color").innerHTML = "Color: Orange";
-    document.getElementById("opacity").innerHTML = "Opacity: 100%";
-    document.getElementById("rotation").innerHTML = "Rotation: 0°";
+    [box.height, box.width, box.backgroundColor, box.opacity] = [initHeight, initWidth, initColor, initOpacity];
+    [sizeText.innerHTML, colorText.innerHTML, opacityText.innerHTML, rotationText.innerHTML]= [initsizeText, initColorText, initOpacityText, initRotationText];
+
+    //reset only by maximum one rotation
+    box.transform = `rotate(${resetDegree()}deg)`;
 }
