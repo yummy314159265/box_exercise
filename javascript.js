a593624344b903bbd28b90f502c2fae839e09354
yummy314159265 <97650924+yummy314159265@users.noreply.github.com>
2022-02-11 04:11:52 -0600

    updated rotation to be smoother

--- javascript.js
+++ javascript.js
@@ -1,29 +1,34 @@
 //make style declarations more readable
 const box = document.getElementById("box").style;
-let [sizeText, colorText, opacityText, rotationText] = [document.getElementById("size"), document.getElementById("color"), document.getElementById("opacity"), document.getElementById("rotation")];
+const [sizeText, colorText, opacityText, rotationText] = [document.getElementById("size"), document.getElementById("color"), document.getElementById("opacity"), document.getElementById("rotation")];
 
 //initial values for reset
 const [initHeight, initWidth, initColor, initOpacity] = [box.height, box.width, box.backgroundColor, box.opacity];
-const [initsizeText, initColorText, initOpacityText, initRotationText] = [sizeText.innerHTML, colorText.innerHTML, opacityText.innerHTML, rotationText.innerHTML];
+const [initSizeText, initColorText, initOpacityText, initRotationText] = [sizeText.innerHTML, colorText.innerHTML, opacityText.innerHTML, rotationText.innerHTML];
 
-//variable and function needed for rotation
+//variables and function needed for rotation
 let randomDegree = 0;
-const resetDegree = () => randomDegree -= randomDegree%360;
+let resetPosition = 0;
+const resetDegree = () => {
+    randomDegree -= randomDegree%90;
+    resetPosition = randomDegree%360;
+    return randomDegree;
+}
 
 //color array
 const colors = ["Orange", "Red", "Blue", "Green", "Yellow", "Purple", "Magenta", "Brown", "Violet"];
 
-//function to make random integers 
-const randomizer = (range=1, initial=0) => Math.floor(range * Math.random()) + initial;
+//make random integers 
+const randomizer = (range=1, min=0) => Math.floor(range * Math.random()) + min;
 
-//Randomly grows/shrinks within a range
+//randomly grows/shrinks box to some size between 25x25px and 500x500px
 document.getElementById("button1").onclick = () =>  {
-    let randomSize = randomizer(475, 25);
+    const randomSize = randomizer(475, 25);
     box.height = box.width = randomSize + "px";
     sizeText.innerHTML = `Size: ${box.height} x ${box.width}`;
 }
 
-//Randomly changes color of box
+//randomly changes color of box
 document.getElementById("button2").onclick = () => {    
     
     //chooses a color from array
@@ -38,31 +43,31 @@ document.getElementById("button2").onclick = () => {
     colorText.innerHTML = `Color: ${randomColor}`;
 }
 
-//Randomly changes opacity of box
+//randomly changes opacity of box
 document.getElementById("button3").onclick = () => {
     const randomOpacity = randomizer(100);
     box.opacity = randomOpacity/100;
     opacityText.innerHTML = `Opacity: ${randomOpacity}%`;
 }
 
-//Randomly rotates box
+//randomly rotates box
 document.getElementById("button4").onclick = () => {
     //set random change in degrees and adds it to global variable, needed to keep box rotating clockwise
     randomDegree += randomizer(180);
     box.transform = `rotate(${randomDegree}deg)`;
-    rotationText.innerHTML = `Rotation: ${randomDegree%360}°`;
+    rotationText.innerHTML = `Rotation: ${(randomDegree-resetPosition)%360}°`;
 }
 
-//Randomizes every element
+//randomizes every element
 document.getElementById("button5").onclick = () => {
     for (let i = 1; i < 5; i++) {document.getElementById(`button${i}`).onclick()}
 }
 
-//Resets box to initial values
+//resets box to initial values
 document.getElementById("button6").onclick = () => {
     [box.height, box.width, box.backgroundColor, box.opacity] = [initHeight, initWidth, initColor, initOpacity];
-    [sizeText.innerHTML, colorText.innerHTML, opacityText.innerHTML, rotationText.innerHTML]= [initsizeText, initColorText, initOpacityText, initRotationText];
+    [sizeText.innerHTML, colorText.innerHTML, opacityText.innerHTML, rotationText.innerHTML] = [initSizeText, initColorText, initOpacityText, initRotationText];
 
-    //reset only by maximum one rotation
+    //resets rotation to the closest rotationally symmetrical position to initial (only works for square, rectangles must be switched to a %180)
     box.transform = `rotate(${resetDegree()}deg)`;
 }
